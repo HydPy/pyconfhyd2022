03bbca8a8a3e912fb08d97d24527df1156c41fc0
Sanchit Balchandani <balchandani.sanchit@gmail.com>
2022-11-13 22:01:16 +0530

    Remove venue details from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,13 +7,6 @@ export const Footer = () => (
         <div class="footer-logo">
             <img src={logo} width={180} height={62} loading="eager" />
         </div>
-        <div class="footer-venue">
-            <p class="footer-venue-heading">Venue</p>
-            <div>
-                <a target="blank" href="https://www.google.com/maps/place/T-Hub/@17.4338577,78.3610584,15z/data=!4m9!1m2!2m1!1sthub!3m5!1s0x3bcb93bd18410b0f:0x8d7e3fea891858ce!8m2!3d17.4338577!4d78.3785679!15sCgR0aHViWgYiBHRodWKSAQRjYW1wmgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVVF3TVV4eGExWm5FQUXgAQA">Conference . </a>
-                <a target="blank" href="https://www.google.com/maps/place/EPAM/@17.4324493,78.3793073,17z/data=!3m1!4b1!4m5!3m4!1s0x3bcb93e152224b79:0xf4ab3bded078553b!8m2!3d17.4324442!4d78.381496">Workshop</a>
-            </div>
-        </div>
         <div class="footer-contact-us">
             <p>Contact-us : contact@hydpy.org</p>
         </div>
