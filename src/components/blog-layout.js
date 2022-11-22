f73e8ecbfd621ab23e83fddf07fe4583dde0ef96
Sanchit Balchandani <balchandani.sanchit@gmail.com>
2022-11-22 15:01:26 +0530

    Update blog-layout.js

--- src/components/blog-layout.js
+++ src/components/blog-layout.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "../assets/stylesheets/application.scss";
 import { Header } from "./header";
 import { Footer } from './footer';
 import bookImage from "../images/faq.webp";
